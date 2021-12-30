1df1f4f14e9050d399ae3e8fcb2b7a37988b28ee
jburke11 <burkej24@msu.edu>
2021-12-30 15:34:32 -0500

    add plotting and data cleanup scripts

--- scripts/cleanup.R
+++ scripts/cleanup.R
@@ -4,7 +4,7 @@ df <- read_tsv("DciA_combined_cln.tsv", col_names = T)
 # get rid of headers from integration
 df <- df[!(df$Query=="Query"),]
 # Label DciA or DnaB
-df <- df %>% mutate(AccNum = if_else(grepl("helicase", STitle), paste0(AccNum, "_DnaB"), paste0(AccNum, "_DciA")))
+df <- df %>% mutate(QueryName = if_else(grepl("helicase", STitle), paste0(QueryName, "_DnaB"), paste0(QueryName, "_DciA")))
 # seperate DNAB
 df2 <- subset(df, grepl("helicase", STitle))
 # seperate DciA/DUF721
@@ -13,14 +13,14 @@ df3 <- subset(df, ! grepl("helicase", STitle))
 in_ipr <- read_tsv("DciA_combined_ipr.tsv", col_names = T)
 
 in_ipr <- in_ipr[!(in_ipr$DB.ID=="DB.ID"),]
-in_ipr <- in_ipr %>% mutate(AccNum = if_else(AccNum %in% df2$AccNum, paste0(AccNum, "_DnaB"), paste0(AccNum, "_DciA")))
+#in_ipr <- in_ipr %>% mutate(QueryName = if_else(AccNum %in% df2$AccNum, paste0(QueryName, "_DnaB"), paste0(QueryName, "_DciA")))
 ipr_2 <- subset(in_ipr, AccNum %in% df2$AccNum)
 ipr_3 <- subset(in_ipr, AccNum %in% df3$AccNum)
 
-write_tsv(df, "all_combined_cln.tsv")
-write_tsv(df2, "DnaB_combined_cln.tsv")
-write_tsv(df3, "DciA_combined_cln.tsv")
+write_tsv(df, "/data/scratch/janani/molevolvr_out/Dciacb_full/cln_combined.tsv")
+write_tsv(df2, "/data/scratch/janani/molevolvr_out/DnaBcm_full/cln_combined.tsv")
+write_tsv(df3, "/data/scratch/janani/molevolvr_out/DciAcm_full/cln_combined.tsv")
 
-write_tsv(in_ipr, "all_combined_ipr.tsv")
-write_tsv(ipr_2, "DnaB_combined_ipr.tsv")
-write_tsv(ipr_3, "DciA_combined_ipr.tsv")
+write_tsv(in_ipr, "/data/scratch/janani/molevolvr_out/Dciacb_full/ipr_combined.tsv")
+write_tsv(ipr_2, "/data/scratch/janani/molevolvr_out/DnaBcm_full/ipr_combined.tsv")
+write_tsv(ipr_3, "/data/scratch/janani/molevolvr_out/DciAcm_full/ipr_combined.tsv")
--- scripts/get_seqs.py
+++ scripts/get_seqs.py
@@ -3,7 +3,6 @@ DnaB = open("/data/scratch/janani/molevolvr_out/DnaBcm_full/DnaBcm.fa", "a")
 DciA = open("/data/scratch/janani/molevolvr_out/DciAcm_full/DciAcm.fa", "a")
 with open("/data/scratch/janani/molevolvr_out/Dciacb_full/Dciacb.fa", "r") as in_file:
     for seq in SeqIO.parse(in_file, "fasta"):
-        print(seq.id)
         if "helicase" in seq.description:
             SeqIO.write(seq, DnaB, "fasta")
         else:
